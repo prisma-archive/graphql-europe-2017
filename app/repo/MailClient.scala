3bec5ae1fdd91362f27b656514ce538dd4345cb0
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2017-01-15 00:23:43 +0100

    Improved email notification

--- app/repo/MailClient.scala
+++ app/repo/MailClient.scala
@@ -4,12 +4,13 @@ import javax.inject.Inject
 
 import com.google.inject.Singleton
 import org.apache.commons.mail.{DefaultAuthenticator, Email, HtmlEmail, SimpleEmail}
-import play.api.Configuration
+import play.api.{Configuration, Logger}
 
 import scala.collection.JavaConverters._
 
 @Singleton
 class MailClient @Inject()(config: Configuration) {
+  val log = Logger(this.getClass)
 
   def send(
     to: String,
@@ -17,22 +18,28 @@ class MailClient @Inject()(config: Configuration) {
     text: String,
     html: String
   ): Unit = {
-    val email = new HtmlEmail()
+    val userName = config.getString("mail.userName").getOrElse("")
 
-    email.setHostName(config.getString("mail.smtpHost").get)
-    email.setSmtpPort(config.getInt("mail.smtpPort").get)
-    email.setAuthenticator(new DefaultAuthenticator(
-      config.getString("mail.userName").get,
-      config.getString("mail.password").get))
-    email.setSSLOnConnect(config.getBoolean("mail.ssl").get)
+    if (userName.trim.nonEmpty) {
+      val email = new HtmlEmail()
 
-    email.setFrom(config.getString("mail.from").get)
-    email.addTo(to)
-    email.setSubject(subject)
-    email.setTextMsg(text)
-    email.setHtmlMsg(html)
+      email.setHostName(config.getString("mail.smtpHost").get)
+      email.setSmtpPort(config.getInt("mail.smtpPort").get)
+      email.setAuthenticator(new DefaultAuthenticator(
+        config.getString("mail.userName").get,
+        config.getString("mail.password").get))
+      email.setSSLOnConnect(config.getBoolean("mail.ssl").get)
 
-    email.send()
+      email.setFrom(config.getString("mail.from").get)
+      email.addTo(to)
+      email.setSubject(subject)
+      email.setTextMsg(text)
+      email.setHtmlMsg(html)
+
+      email.send()
+    } else {
+      log.error("SMTP is not configured!")
+    }
   }
 
 }
