3195ba5793bce0d55f32bd2689581a2b90307b5d
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2017-01-14 19:59:01 +0100

    improved logic

--- app/controllers/Hooks.scala
+++ app/controllers/Hooks.scala
@@ -2,20 +2,37 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.libs.json.Json
+import play.api.libs.json.{JsString, Json}
 import play.api.{Configuration, Logger}
 import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.Future
 
 class Hooks @Inject() (config: Configuration) extends Controller {
   val log = Logger(this.getClass)
 
-  def subscriberCreated = Action { req ⇒
+  def subscriberCreated = Action.async { req ⇒
     req.body.asJson.map { body ⇒
-      log.info("Body json: " + Json.stringify(body))
+      (body \ "createdNode" \ "id") match {
+        case JsString(id) ⇒
+          try {
+            sendSubscriptionConfirmation(id)
 
-      Ok("Thanks!")
+            Future.successful(Ok("Done"))
+          } catch {
+            case e: Exception ⇒
+              log.error(s"Can't send subscription notificatrion for ID '$id'.", e)
+              Future.successful(Ok("Oops. Something went wrong."))
+          }
+
+        case _ ⇒ Future.successful(BadRequest("Wrong Json data shape"))
+      }
     }.getOrElse {
-      BadRequest("Expecting Json data")
+      Future.successful(BadRequest("Expecting Json data"))
     }
+  }
+
+  private def sendSubscriptionConfirmation(id: String): Unit = {
+    log.info("ID: " + id)
   }
 }
