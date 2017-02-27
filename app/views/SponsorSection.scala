3beea87270ade90f1e592391d1991d1e2b8a30d8
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2017-02-27 16:08:58 +0100

    Design improvements

--- app/views/SponsorSection.scala
+++ app/views/SponsorSection.scala
@@ -1,13 +1,13 @@
 package views
 
-case class SponsorSection(name: String, sponsorType: SponsorType.Value, color: String)
+case class SponsorSection(name: String, sponsorType: SponsorType.Value, color: String, borderColor: String)
 
 object SponsorSection {
   val sections = List(
-    SponsorSection("Platinum", SponsorType.Platinum, "#AAACAA"),
-    SponsorSection("Gold", SponsorType.Gold, "#FA8D22"),
-    SponsorSection("Silver", SponsorType.Silver, "#331A99"),
-    SponsorSection("Bronze", SponsorType.Bronze, "#F56199"),
-    SponsorSection("Co-Organisers", SponsorType.Organiser, "#1f2228")
+    SponsorSection("Platinum", SponsorType.Platinum, "#AAACAA", "rgba(170, 172, 170, 0.25)"),
+    SponsorSection("Gold", SponsorType.Gold, "#FA8D22", "rgba(250, 141, 34, 0.25)"),
+    SponsorSection("Silver", SponsorType.Silver, "#331A99", "rgba(51, 26, 153, 0.25)"),
+    SponsorSection("Bronze", SponsorType.Bronze, "#F56199", "rgba(245, 97, 153, 0.25)"),
+    SponsorSection("Co-Organisers", SponsorType.Organiser, "#1f2228", "#c9ced6")
   )
 }
